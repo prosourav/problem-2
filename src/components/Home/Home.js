b0a1d9af94a1939836a3f8e0e4b35484a984e934
prosourav <sourav19961010@gmail.com>
2021-07-10 00:31:22 +0530

    task complete

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,7 +3,7 @@ import Cards from '../Card/Cards';
 const Home = () => {
    const [pageOne,setpageOne] = useState([]);
    const [pageTwo,setpageTwo] = useState([]);
-   const [page,setpage]=useState('page1');
+   const [page,setpage]=useState('Page-1');
    useEffect(()=>{
       fetch('https://reqres.in/api/users?page=1')
       .then(res=>res.json())
@@ -22,15 +22,18 @@ const Home = () => {
 
    return (
       <div className='container d-flex justify-content-center'>
+     
       <div className='row mt-5 '>
-         {page==='page1'?
-         pageOne.map(page=><Cards page={page} key={page.id}></Cards>):
-         pageTwo.map(page=><Cards page={page} key={page.id}></Cards>)
-         }
-         <div className='text-center mt-5'>
-         <button className='mx-1 bg-primary text-white' onClick={()=>setpage('page1')}>page1</button>
-         <button className='mx-1 bg-primary text-white' onClick={()=>setpage('page2')}>page2</button>
+      <h6 className='text-center'>{page}</h6>
+         {page==='Page-1'?
+         pageOne.map(page=><Cards page={page} key={page.id} ></Cards>):
+         pageTwo.map(page=><Cards page={page} key={page.id} ></Cards>)
+   }
+  -       <div className='text-center mt-3'>
+         <button className='mx-1 bg-primary text-white' onClick={()=>setpage('Page-1')}>page1</button>
+         <button className='mx-1 bg-primary text-white' onClick={()=>setpage('Page-2')}>page2</button>
          </div>
+         
       </div>
       </div>
    );
